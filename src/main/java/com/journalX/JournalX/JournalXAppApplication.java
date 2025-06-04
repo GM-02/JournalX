af450da7cca900258aa42cdab3d6fe6a3b3507b0
GM-02 <gm.malik.cs@gmail.com>
2025-06-04 12:33:59 -0700

    basic

---
+++ src/main/java/com/journalX/JournalX/JournalXAppApplication.java
@@ -0,0 +1,13 @@
+package com.journalX.JournalX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalXAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalXAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/journalX/JournalX/JournalXAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.journalX.JournalX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalXAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
